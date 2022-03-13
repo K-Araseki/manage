017d28cec77388e8431be000a939c975a43675f5
Atom-T <b2191560@photon.chitose.ac.jp>
2022-03-13 21:49:21 +0900

    TeacherReportPage.java 修正した

--- manage/src/main/java/com/example/manage/page/teacher/TeacherReportPage.java
+++ manage/src/main/java/com/example/manage/page/teacher/TeacherReportPage.java
@@ -4,7 +4,7 @@ import org.apache.wicket.markup.html.WebPage;
 import org.wicketstuff.annotation.mount.MountPath;
 
 @MountPath("TeacherReport")
-public class TeacherReportPage {
+public class TeacherReportPage extends WebPage{
 
     public TeacherReportPage(){
 
